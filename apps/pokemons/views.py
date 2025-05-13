a2d3ad9c444c1bdd1459ee9082bc1e09da742539
Alexey Londkevich <londkevich@gmail.com>
2025-05-13 16:17:44 +0200

    Add list view for Pokemons with filtering, pagination, and HTMX integration.

--- apps/pokemons/views.py
+++ apps/pokemons/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django_filters.views import FilterView
 
-# Create your views here.
+from pokemons.models import Pokemon
+from pokemons.filters import PokemonFilter
+
+class PokemonListView(FilterView, ListView):
+    """
+    HTMX-based list view for Pokemon model with filtering and pagination.
+    """
+    model = Pokemon
+    template_name = 'pokemons/pokemons.html'
+    context_object_name = 'pokemons'
+    paginate_by = 30
+    filterset_class = PokemonFilter
+
+    def get_template_names(self):
+        """
+        Return different template names based on whether the request is an HTMX request.
+        """
+        if self.request.headers.get('HX-Request'):
+            return ['pokemons/pokemon_list.html']
+        return [self.template_name]
